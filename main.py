c9d4f04d645e1ce293eff8e1adc60db9b835a60f
RoryMO <120664694+RoryMO@users.noreply.github.com>
2023-06-29 11:44:43 -0400

    Reads excel, outputs in Run terminal

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
-# This is a sample Python script.
+import os
+import openpyxl
+from tabulate import tabulate
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def process_excel_files(folder_path):
+    # Iterate over files in the folder
+    for file_name in os.listdir(folder_path):
+        # Check if the file is an Excel file
+        if file_name.endswith('.xlsx') or file_name.endswith('.xls'):
+            # Construct the full file path
+            file_path = os.path.join(folder_path, file_name)
+
+            # Load the Excel file
+            workbook = openpyxl.load_workbook(file_path)
+
+            # Print the sheet names
+            print(f"File: {file_name}")
+            print("Sheet Names:")
+            for sheet_name in workbook.sheetnames:
+                print(sheet_name)
+
+            # Iterate over sheets and print cell values
+            for sheet in workbook:
+                print(f"\nSheet: {sheet.title}")
+                data = []
+                for row in sheet.iter_rows(values_only=True):
+                    filtered_row = [cell_value if cell_value is not None else "" for cell_value in row]
+                    data.append(filtered_row)
+                headers = data[0]
+                data = data[1:]
+                print(tabulate(data, headers=headers, tablefmt="grid"))
+
+            # Close the workbook
+            workbook.close()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Provide the path to the folder containing the Excel files
+folder_path = r'C:\Users\roneill\Documents\CRO'
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Call the function to process Excel files in the folder
+process_excel_files(folder_path)
