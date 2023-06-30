3edcab8ffc8bd8f434de4d125119c600ab7eb0d2
RoryMO <120664694+RoryMO@users.noreply.github.com>
2023-06-30 10:34:57 -0400

    Reformatted code

--- cable_classes.py
+++ cable_classes.py
@@ -1,8 +1,11 @@
 # This is the cable class to store each individual cable's set of information
 class Cable:
-    def __init__(self, pull_number, stationing_start, stationing_end, cable_size, is_express):
+    def __init__(self, pull_number, stationing_start, stationing_end, cable_size, express):
         self.pull_number = pull_number
         self.stationing_start = stationing_start
         self.stationing_end = stationing_end
         self.cable_size = cable_size
-        self.is_express = is_express
+        self.express = express
+
+
+cable_list = []
--- main.py
+++ main.py
@@ -1,40 +1,6 @@
-import os
-import openpyxl
-from tabulate import tabulate
+from file_handler import obtain_cable_data
 
-def process_excel_files(folder_path):
-    # Iterate over files in the folder
-    for file_name in os.listdir(folder_path):
-        # Check if the file is an Excel file
-        if file_name.endswith('.xlsx') or file_name.endswith('.xls'):
-            # Construct the full file path
-            file_path = os.path.join(folder_path, file_name)
+# List to store all cables from pull sheet
 
-            # Load the Excel file
-            workbook = openpyxl.load_workbook(file_path)
-
-            # Print the sheet names
-            print(f"File: {file_name}")
-            print("Sheet Names:")
-            for sheet_name in workbook.sheetnames:
-                print(sheet_name)
-
-            # Iterate over sheets and print cell values
-            for sheet in workbook:
-                print(f"\nSheet: {sheet.title}")
-                data = []
-                for row in sheet.iter_rows(values_only=True):
-                    filtered_row = [cell_value if cell_value is not None else "" for cell_value in row]
-                    data.append(filtered_row)
-
-                print(tabulate(data, tablefmt="grid"))
-
-            # Close the workbook
-            workbook.close()
-
-
-# Provide the path to the folder containing the Excel files
-folder_path = r'C:\Users\roneill\Documents\CRO'
-
-# Call the function to process Excel files in the folder
-process_excel_files(folder_path)
+#print("hello")
+obtain_cable_data()
