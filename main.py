d3fe4d5359e55c5f7da08a787d7f17d755485691
RoryMO <120664694+RoryMO@users.noreply.github.com>
2023-08-03 16:05:03 -0400

    inti comt

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from user_interface import *
 from messenger_algorithm import *
 from cable_classes import *
 from visualizer import get_cable_pull_sheet
+import random
 
 get_cable_pull_sheet()
 get_cable_sizes()      # Repository of all cables and their parameters
@@ -16,8 +17,9 @@ stationing_sections()  # List cables between each section of stationing
 # print(cable_list[5].cable_size)
 # print("Number of cables in queue:", len(draw_queue))
 
-print(cable_list[0].cable_size)
-add_to_draw_queue(cable_list[0], 0, 0)
+for i in range(random.randint(0, 22)):
+    print(cable_list[i].cable_size)
+    add_to_draw_queue(cable_list[i], random.randint(0, 10), random.randint(0, 360))
 generate_cable_image(draw_queue)
 
 # Generate the final image with all the cables to be drawn
