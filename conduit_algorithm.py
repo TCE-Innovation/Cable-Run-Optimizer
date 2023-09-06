cd90a6eee3753c9a875b995a5ab13ecdebcaa2a2
RoryMO <120664694+RoryMO@users.noreply.github.com>
2023-09-06 14:16:27 -0400

    Cable Sizing + Iterating Properly

--- conduit_algorithm.py
+++ conduit_algorithm.py
@@ -20,9 +20,9 @@ def optimize_for_conduit():
                 cables_within_range.append(cable)
 
         # Separate express and local cables
-        # Conduits are
         express_cables = []
         local_cables = []
+
         for cable in cables_within_range:
 
             if cable.express.lower() == "express":
@@ -34,10 +34,13 @@ def optimize_for_conduit():
         express_cables.sort(key=lambda cable: cable.cross_sectional_area, reverse=True)
         local_cables.sort(key=lambda cable: cable.cross_sectional_area, reverse=True)
 
+        print()
         # Create conduits, express cables first, then local cables
         if len(express_cables):
+            print("Function call of create_conduits to create conduits from express cables...")
             create_conduits(express_cables, start_stationing, end_stationing, True)
         if len(local_cables):
+            print("Function call of create_conduits to create conduits from local cables...")
             create_conduits(local_cables, start_stationing, end_stationing, False)
 
 
@@ -64,23 +67,61 @@ def create_conduits(cables_within_range, start_stationing, end_stationing, expre
 
     # Initialize a list to keep track of which cables are being placed in a conduit
     # to avoid double counting cables across conduits
-    placed_cables =[]
+    placed_cables = []
+
+    print("Pull Numbers of Cables within the stationing range to create conduits from:")
+    pull_numbers = ", ".join(str(cable.pull_number) for cable in cables_within_range)
+    print(pull_numbers)
+    print()
 
     # Go through all cables in the stationing range
     for cable in cables_within_range:
+        print()
+        print(f"Cable in outermost for loop: {cable.pull_number}")
+
+        if cable in placed_cables:
+            continue
+
         # Check if cable added to current conduit wouldn't violate free air space requirement
         if check_free_air_space(conduit, cable):
             find_open_space(conduit, cable)
             placed_cables.append(cable)
-        # else if cable can't fit, then try other cables before creating new conduit image
+            print(f"Cable {cable.pull_number} was placed.")
+            print(f"Conduit {conduit_number} Fill: {100 - conduit.conduit_free_air_space:.2f}%")
+            print()
 
+        # else if cable can't fit, then try other cables before creating new conduit image
         else:
+            print(f"Cable {cable.pull_number} NOT placed. Cable Area: {cable.cross_sectional_area}")
+            print("Checking if a smaller cable can fit into the conduit...")
+            # Create a list of smaller cables that can fit into the conduit
+            smaller_cables = [c for c in cables_within_range if
+                              c not in placed_cables and c.cross_sectional_area < cable.cross_sectional_area]
+
+            print("Small cables that will be tested if they fit:")
+            for small_cable in smaller_cables:
+                print(f"Pull #: {small_cable.pull_number}, Area: {small_cable.cross_sectional_area}")
+
+            # Iterate through the smaller cables
+            for smaller_cable in smaller_cables:
+                print(f"Small cable being tested: {smaller_cable.pull_number}...")
+                # Check if the smaller cable can fit without violating free air space
+                if check_free_air_space(conduit, smaller_cable):
+                    print(f"Pass. Cable {smaller_cable.pull_number} can into Conduit {conduit_number}")
+                    # If it can fit, find an open space in the conduit for the smaller cable
+                    find_open_space(conduit, smaller_cable)
+
+                    # Add the smaller cable to the list of placed cables to avoid double placement
+                    placed_cables.append(smaller_cable)
+                else:
+                    print(f"Fail. Cable {smaller_cable.pull_number} cannot fit into Conduit {conduit_number}")
+
+
             # Draw image, reset draw queue, increment conduit number printed onto next image
             generate_cable_image(draw_queue)    # Create full conduit image
             draw_queue.clear()                  # Empty draw queue for next image
             conduit_number += 1                 # Identifier for image
             conduit_name = "Conduit" + str(conduit_number)
-
             conduit = Conduit(start_stationing, end_stationing, conduit_free_air_space)
             conduits[conduit_name] = conduit
 
@@ -99,22 +140,24 @@ def create_conduits(cables_within_range, start_stationing, end_stationing, expre
 def check_free_air_space(conduit, cable):
     global conduit_free_air_space
     total_area = 0
-    for cable in conduit.cables:
-        total_area += cable.cross_sectional_area
+    for existing_cable in conduit.cables:
+        total_area += existing_cable.cross_sectional_area
 
-    # print("Total current area taken up in the conduit:", round(total_area, 2))
+    print(f"Testing if cable {cable.pull_number} can fit into Conduit {conduit_number}...")
+
     total_area += cable.cross_sectional_area
-    print(f'Area: {total_area}')
+    # print(f'Area: {total_area}')
 
     # If area taken up by all cables in conduit is less than the maximum area that can be taken up by cable
     if total_area / (math.pi * (conduit_size/2) ** 2) < (1-free_air_space_requirement):
         # Update free airspace value for conduit
         conduit_free_air_space = round((1 - (total_area / (math.pi * ((conduit_size/2) ** 2)))) * 100, 2)
         conduit.conduit_free_air_space = conduit_free_air_space
-        print(f'Conduit Fill: {100-conduit_free_air_space}')
+        # print(f"Conduit Fill: {100 - conduit.conduit_free_air_space:.2f}%")
         return 1
     else:
         # Return 0 for outside of if statement to check other cables/make next conduit
+        # print(f"Area failed for adding cable {cable.pull_number}. Theoretical Fill: {100*(total_area / (math.pi * (conduit_size/2) ** 2)):.2f}%")
         return 0
 
 
